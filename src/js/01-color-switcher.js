6bdefb0978f7765d6b469be8d7196856f9619679
Maksym150678 <civicm110@gmail.com>
2022-07-14 17:55:07 +0300

    hw9 tk1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let intervalId = null;
+stopButton.disebled = true;
+
+startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+
+    intervalId = setInterval(() => {
+        body.style.background = getRandomHexColor()
+    }, 1000);
+});
+stopButton.addEventListener('click', () => {
+   clearInterval(intervalId);
+   startButton.disabled = false;
+   stopButton.disabled = true;
+});
+
+
 
